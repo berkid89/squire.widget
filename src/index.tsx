5cd623f2cfe0ea4e91fdf80ab069bafa082b4bb1
Berki Daniel <daniel.berki@jaystack.com>
2018-05-04 15:53:06 +0200

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import App from './App';
+import rootReducer from './reducers';
+import registerServiceWorker from './registerServiceWorker';
+
+import './index.css';
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root') as HTMLElement
+);
+registerServiceWorker();
